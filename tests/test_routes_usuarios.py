20cae90454e4e34200edb9e2ecb97ddd8e1a6c54
erkbritto <erickbritto060@gmail.com>
2025-09-26 15:55:37 -0300

    fix: melhorando o sistema com testes

--- tests/test_routes_usuarios.py
+++ tests/test_routes_usuarios.py
@@ -1,8 +1,5 @@
 import pytest
-import sys
-import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../my-flask-app')))
-from main import app  # Change 'main' to the actual filename (without .py) where your Flask app is defined
+from routes.routes import app
 
 @pytest.fixture
 def client():
@@ -13,16 +10,14 @@ def client():
 def test_listar_usuarios(client):
     response = client.get('/usuarios/listar')
     assert response.status_code == 200
-    assert isinstance(response.json['usuarios'], list)
+    assert isinstance(response.json, list)
 
 def test_cadastrar_usuario(client):
     novo_usuario = {
-        "nome": "testeuser",
-        "email": "testeuser@email.com",
-        "senha": "123456",
-        "cargo": "FUNCIONARIO",
+        "usuario": "testeuser",
+        "cargo": "funcionario",
         "departamento": "TI",
-        "status": "ATIVO"
+        "status": "ativo"
     }
     response = client.post('/usuarios/cadastrar', json=novo_usuario)
     assert response.status_code in [200, 201, 400]  # 400 se já existir ou erro de validação
